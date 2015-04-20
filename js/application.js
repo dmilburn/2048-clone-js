84b06d5da1b58750525f43bec04cbd252ed0e427
Debbie Milburn & Lucian Kahn <pair+dmilburn.luciankahn@devbootcamp.com>
2015-04-20 18:55:06 -0400

    add preventDefault to mousetrap keybindings

--- js/application.js
+++ js/application.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
   Board.newGame();
 
   ["up", "down", "right", "left"].forEach(function(direction) {
-    Mousetrap.bind(direction, function(){
+    Mousetrap.bind(direction, function(event){
+    event.preventDefault();
     board.move(direction);
     board.draw();
     })
