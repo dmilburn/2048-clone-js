d27a0225d63f352cd13c4b52fa139a8223a4f653
Debbie Milburn & Lucian Kahn <pair+dmilburn.luciankahn@devbootcamp.com>
2015-04-20 18:53:06 -0400

    refactor keybindings

--- js/application.js
+++ js/application.js
@@ -2,26 +2,12 @@ $(document).ready(function() {
 
   Board.newGame();
 
-  Mousetrap.bind("up", function(){
-    board.move("up");
+  ["up", "down", "right", "left"].forEach(function(direction) {
+    Mousetrap.bind(direction, function(){
+    board.move(direction);
     board.draw();
-  })
-  Mousetrap.bind("down", function(){
-    board.move("down");
-    board.draw();
-  })
-  Mousetrap.bind("left", function(){
-    board.move("left");
-    board.draw();
-  })
-  Mousetrap.bind("right", function(){
-    board.move("right");
-    board.draw();
-  })
-
-  $('#new-game').on('click', function() {
-    Board.newGame();
-  })
+    })
+  });
 
 });
 
