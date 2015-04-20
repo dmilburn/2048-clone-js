8324fef1233f5ba963df6745dab9d09d14e0087d
Debbie Milburn & Lucian Kahn <pair+dmilburn.luciankahn@devbootcamp.com>
2015-04-20 16:26:24 -0400

    create html table to contain game and set basic css

--- js/application.js
+++ js/application.js
@@ -2,20 +2,4 @@ $(document).ready(function() {
 
 board = new Board('0000000000020002');
 
-board.setUp();
-board.toString();
-// board.toString();
-
-// board.moveDown();
-
-// board.toString();
-
-// board.moveUp();
-// board.toString();
-// board.moveDown();
-// board.toString();
-// board.moveRight();
-// board.toString();
-board.moveUp();
-board.toString();
 });
