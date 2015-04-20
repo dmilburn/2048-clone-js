4aadd3b76f65ecbb8cc387c527176501bedb434e
Debbie Milburn & Lucian Kahn <pair+dmilburn.luciankahn@devbootcamp.com>
2015-04-20 11:33:09 -0400

    debug accidentally cloning arrays in Board initialization; toString is working for testing

--- js/game.js
+++ js/game.js
@@ -6,25 +6,19 @@ function Square(number) {
 
 function Board(string) {
   this.input = string;
-  this.board = [];
-  var smallArray = [new Square(0), new Square(0), new Square(0), new Square(0)];
-  for (i=0; i<4; i++) {
-    this.board.push(smallArray);
-  }
+  this.board = [[new Square(0), new Square(0), new Square(0), new Square(0)],
+                [new Square(0), new Square(0), new Square(0), new Square(0)],
+                [new Square(0), new Square(0), new Square(0), new Square(0)],
+                [new Square(0), new Square(0), new Square(0), new Square(0)]];
 }
 
 Board.prototype.setUp = function() {
   var initialValues = this.input.split("");
-  // find the index of values are NOT 0.
   for (i=0; i<initialValues.length; i++) {
-      // find index of the value IF it's not zero
       var numToTest = parseInt(initialValues[i]);
     if (numToTest !== 0) {
-      // convert that index to the index of this.board
       var innerArrayIndex = i%4;
       var outerArrayIndex = parseInt(i/4);
-    // change the number property of the square at that index
-    //   to the value from the input string.
       this.board[outerArrayIndex][innerArrayIndex].number = parseInt(initialValues[i]);
     }
   }
@@ -32,7 +26,15 @@ Board.prototype.setUp = function() {
 
 
 Board.prototype.toString = function(){
-  console.log(_.flatten(this.board));
+  var flattenedBoard = _.flatten(this.board);
+  // console.log(flattenedBoard);
+  for (i=0; i<flattenedBoard.length; i++){
+    flattenedBoard[i] = flattenedBoard[i].number.toString();
+    if (i%4 === 3){
+      flattenedBoard[i] += "\n";
+    }
+  }
+  console.log(flattenedBoard.toString());
 }
 
 
